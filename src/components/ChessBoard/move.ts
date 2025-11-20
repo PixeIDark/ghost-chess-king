d20334f073a7b14658819e2217cb63c734f07838
PixeIDark <kiki7951@naver.com>
2025-11-21 06:42:37 +0900

    feat: 기물 움직임 적용 함수 구현

---
+++ src/components/ChessBoard/move.ts
@@ -0,0 +1,9 @@
+import type { Board } from "../../constants/board.ts";
+
+export const movePiece = (board: Board, fromRow: number, fromCol: number, toRow: number, toCol: number): Board => {
+  const newBoard = structuredClone(board);
+  newBoard[toRow][toCol] = newBoard[fromRow][fromCol];
+  newBoard[fromRow][fromCol] = null;
+
+  return newBoard;
+};
