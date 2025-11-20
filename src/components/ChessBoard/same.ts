bf84c833e7bf7560f868cc601e1312ae60837c14
PixeIDark <kiki7951@naver.com>
2025-11-21 06:45:07 +0900

    feat: 아군 기물인지 여부 반환 함수 구현

---
+++ src/components/ChessBoard/same.ts
@@ -0,0 +1,6 @@
+import type { Piece } from "../../constants/board.ts";
+
+export const isSameColor = (piece1: Piece | null, piece2: Piece | null): boolean => {
+  if (!piece1 || !piece2) return false;
+  return piece1.color === piece2.color;
+};
