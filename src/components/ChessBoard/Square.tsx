3026a57dadcb7379c2f8a2141b73596644a4b97d
PixeIDark <kiki7951@naver.com>
2025-11-22 01:12:39 +0900

    feat: 기물 포커싱 및 이동 가능 위치 표시, 기물 ui text => svg

--- src/components/ChessBoard/Square.tsx
+++ src/components/ChessBoard/Square.tsx
@@ -1,29 +1,54 @@
 import type { Piece } from "../../constants/board.ts";
 
-const colors = {
-  white: "bg-white text-black",
-  black: "bg-black text-white",
-  none: "",
-};
-
 interface SquareProps {
   square: Piece | null;
   onSquareClick: (row: number, col: number) => void;
   rowIndex: number;
   colIndex: number;
+  isSelected: boolean;
+  canMoveSquare: boolean;
 }
 
-function Square({ square, onSquareClick, rowIndex, colIndex }: SquareProps) {
-  const type = square?.type ?? null;
-  const color = colors[square?.color ?? "none"];
+const pieceImages = {
+  pawn: {
+    white: "/pieces/white_pawn.svg",
+    black: "/pieces/black_pawn.svg",
+  },
+  knight: {
+    white: "/pieces/white_knight.svg",
+    black: "/pieces/black_knight.svg",
+  },
+  bishop: {
+    white: "/pieces/white_bishop.svg",
+    black: "/pieces/black_bishop.svg",
+  },
+  rook: {
+    white: "/pieces/white_rook.svg",
+    black: "/pieces/black_rook.svg",
+  },
+  queen: {
+    white: "/pieces/white_queen.svg",
+    black: "/pieces/black_queen.svg",
+  },
+  king: {
+    white: "/pieces/white_king.svg",
+    black: "/pieces/black_king.svg",
+  },
+} as const;
+
+function Square({ square, onSquareClick, rowIndex, colIndex, isSelected, canMoveSquare }: SquareProps) {
+  const floorColor = (rowIndex + colIndex) % 2 === 0 ? "bg-amber-800" : "bg-amber-100";
+  const piece = square ? pieceImages[square.type][square.color] : null;
+  const selectedSquareBorder = isSelected ? "border-4 border-blue-500" : "";
+  const moveSquareBorder = canMoveSquare ? "border-4 border-green-500" : "";
 
   return (
     <button
-      className={`flex h-14 w-14 cursor-pointer flex-col items-center justify-center border border-blue-200 ${color}`}
+      className={`flex h-14 w-14 cursor-pointer flex-col items-center justify-center text-3xl ${floorColor} ${selectedSquareBorder} ${moveSquareBorder}`}
       type="button"
       onClick={() => onSquareClick(rowIndex, colIndex)}
     >
-      <p>{type}</p>
+      {piece && <img src={piece} alt={`${square?.color}-${square?.type}`} />}
     </button>
   );
 }
