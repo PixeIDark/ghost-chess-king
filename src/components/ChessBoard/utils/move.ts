8c1a7bb51abbe61a7933896586f53ddee83a28a8
PixeIDark <kiki7951@naver.com>
2025-11-23 18:56:51 +0900

    refactor: 이동 시, 앙파상 및 캐슬링 검증 그리고 기물 상태 관리

--- src/components/ChessBoard/utils/move.ts
+++
@@ -1,9 +0,0 @@
-import type { Board } from "../../../constants/board.ts";
-
-export const movePiece = (board: Board, fromRow: number, fromCol: number, toRow: number, toCol: number): Board => {
-  const newBoard = structuredClone(board);
-  newBoard[toRow][toCol] = newBoard[fromRow][fromCol];
-  newBoard[fromRow][fromCol] = null;
-
-  return newBoard;
-};
