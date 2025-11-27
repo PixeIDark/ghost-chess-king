61b6ae62f8bfb005eb38f88f5fe15b5d1c6309b6
PixeIDark <kiki7951@naver.com>
2025-11-27 20:56:53 +0900

    feat: 체스 도메인 타입 정의
    - PieceName 타입: 체스 기물 종류
    - Side 타입: 플레이어 색상 (흑/백)
    - Piece 인터페이스: 기물 타입과 색상
    - Cell 타입: 기물 또는 빈칸 (null)
    - Board 타입: 8x8 Cell 배열
    - File, Rank 타입: 체스 표기법 (a-h, 1-8)
    - Square 타입: 템플릿 리터럴로 위치 표현 (예: 'e4')
    - BoardIndex 타입: 배열 인덱스 (0-7)

---
+++ client/src/types/chess.ts
@@ -0,0 +1,17 @@
+export type PieceName = "pawn" | "knight" | "bishop" | "rook" | "queen" | "king";
+
+export type Side = "black" | "white";
+
+export interface Piece {
+  type: PieceName;
+  color: Side;
+}
+
+export type Cell = Piece | null;
+
+export type Board = Cell[][];
+
+export type Rank = "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8";
+export type File = "a" | "b" | "c" | "d" | "e" | "f" | "g" | "h";
+export type Square = `${File}${Rank}`;
+export type BoardIndex = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7;
