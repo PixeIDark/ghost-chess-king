ec974b6b094be27c5944daf738d1b902edb83652
PixeIDark <kiki7951@naver.com>
2026-01-09 00:53:23 +0900

    fix: vite.config tailwindcss 빌드오류 수정

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import * as path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   worker: {
@@ -10,5 +10,11 @@ export default defineConfig({
   },
   optimizeDeps: {
     exclude: ["stockfish"],
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@ghost-chess-king/shared": path.resolve(__dirname, "../../packages/shared/src/index.ts"),
+    },
   },
 });
