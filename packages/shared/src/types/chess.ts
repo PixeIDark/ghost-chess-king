957f8a91c22c4afa792ebb4590807a4a6f028960
PixeIDark <kiki7951@naver.com>
2026-01-13 23:41:51 +0900

    fix: 타입 오류 수정

--- packages/shared/src/types/chess.ts
+++ packages/shared/src/types/chess.ts
@@ -11,8 +11,8 @@ export interface Piece {
 
 export type Cell = Piece | null;
 
-export type BoardDTO = IPieceData | null[][];
-export type BoardEntity = IPiece | null[][];
+export type BoardDTO = (IPieceData | null)[][];
+export type BoardEntity = (IPiece | null)[][];
 
 export type File = "a" | "b" | "c" | "d" | "e" | "f" | "g" | "h";
 export type Rank = "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8";
