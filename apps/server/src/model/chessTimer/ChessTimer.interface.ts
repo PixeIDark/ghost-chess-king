85204914a823540a5da91ee04c054f2bf477e23a
PixeIDark <kiki7951@naver.com>
2026-01-13 23:32:07 +0900

    chore: 레거시 파일 제거

--- apps/server/src/model/chessTimer/ChessTimer.interface.ts
+++
@@ -1,8 +0,0 @@
-import { Side } from "@ghost-chess-king/shared";
-
-export interface IChessTimer {
-  start(startTurn: Side): void;
-  switchTurn(nextTurn: Side): void;
-  stop(): void;
-  getTime(): { whiteTime: number; blackTime: number };
-}
--- apps/server/src/model/chessTimer/ChessTimer.ts
+++ apps/server/src/model/chessTimer/ChessTimer.ts
@@ -1,6 +1,6 @@
 import { Side } from "@ghost-chess-king/shared";
 import { INCREMENT_TIME, INITIAL_TIME } from "@/model/chessTimer/ChessTimer.constants";
-import { IChessTimer } from "@/model/chessTimer/ChessTimer.interface";
+import { IChessTimer } from "../../../../../packages/shared/src/types/models/ChessTimer.interface";
 
 export class ChessTimer implements IChessTimer {
   private whiteTime: number;
