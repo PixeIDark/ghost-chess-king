d4a3ed02686eb1a750c9996535792f603e7c4beb
PixeIDark <kiki7951@naver.com>
2026-01-17 23:11:56 +0900

    feat: ai 캐슬룩 구현

--- apps/client/src/pages/game/hooks/useAi.ts
+++ apps/client/src/pages/game/hooks/useAi.ts
@@ -1,7 +1,6 @@
+import { Position, PromotionPieceName, squareToIndices } from "@ghost-chess-king/shared";
+import { useStockfish } from "@/hooks/useStockfish.ts";
 import { useEffect } from "react";
-import { useStockfish } from "@/hooks/useStockfish";
-import { PromotionPieceName, squareToIndices } from "@ghost-chess-king/shared";
-import type { Position } from "@ghost-chess-king/shared";
 
 interface UseAiParams {
   fen: string;
@@ -18,6 +17,19 @@ const promotionTypes = {
   k: "knight",
 } as const satisfies Record<string, PromotionPieceName>;
 
+const isCastlingMove = (from: Position, to: Position): boolean => {
+  const isKingRow = from.row === 0 || from.row === 7;
+  const isKingStartCol = from.col === 4;
+  const horizontalDiff = Math.abs(to.col - from.col);
+
+  return isKingRow && isKingStartCol && horizontalDiff === 2;
+};
+
+const convertCastlingMove = (from: Position, to: Position, cols: number): Position => {
+  if (to.col > from.col) return { row: to.row, col: cols - 1 };
+  else return { row: to.row, col: 0 };
+};
+
 export const useAi = ({ fen, currentTurn, aiSide, depth = 15, onAiMove }: UseAiParams) => {
   const { isReady, getBestMove } = useStockfish();
 
@@ -29,15 +41,18 @@ export const useAi = ({ fen, currentTurn, aiSide, depth = 15, onAiMove }: UseAiP
     (async () => {
       try {
         const bestMove = await getBestMove(fen, depth);
-        console.log("bestMove", bestMove);
         if (bestMove === "(none)") return;
 
         const from = squareToIndices(bestMove.slice(0, 2));
-        const to = squareToIndices(bestMove.slice(2, 4));
+        let to = squareToIndices(bestMove.slice(2, 4));
         const promotionChar = bestMove[4];
         let promotionType = null;
 
-        if (promotionChar in promotionTypes) promotionType = promotionChar;
+        if (isCastlingMove(from, to)) to = convertCastlingMove(from, to, 8);
+
+        if (promotionChar && promotionChar in promotionTypes) {
+          promotionType = promotionTypes[promotionChar as keyof typeof promotionTypes];
+        }
 
         if (!cancelled) onAiMove(from, to, promotionType);
       } catch (err) {
