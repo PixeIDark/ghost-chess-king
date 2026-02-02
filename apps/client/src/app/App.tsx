f317feb099a22865bb072bbe512c3a2c6ce1883a
PixeIDark <kiki7951@naver.com>
2026-02-03 01:02:15 +0900

    refactor: 라우트 중복 제거

--- apps/client/src/app/App.tsx
+++ apps/client/src/app/App.tsx
@@ -1,16 +1,12 @@
 import { router } from "../route/router.ts";
 import { RouterProvider } from "react-router";
 import { SocketProvider } from "../contexts/SessionContext.tsx";
-import ServerStatus from "@/components/ServerStatus.tsx";
 import "./global.css";
 
 function App() {
   return (
     <SocketProvider>
-      <div className="flex h-screen flex-col items-center">
-        <ServerStatus />
-        <RouterProvider router={router}></RouterProvider>
-      </div>
+      <RouterProvider router={router}></RouterProvider>
     </SocketProvider>
   );
 }
