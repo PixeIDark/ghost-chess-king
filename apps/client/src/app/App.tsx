a8824e6c7c9a002a2ac4d81baf5d536fe1094ab1
PixeIDark <kiki7951@naver.com>
2026-02-03 22:23:10 +0900

    chore: 소켓프로바이더 위치 변경

--- apps/client/src/app/App.tsx
+++ apps/client/src/app/App.tsx
@@ -1,14 +1,9 @@
 import { router } from "../route/router.ts";
 import { RouterProvider } from "react-router";
-import { SocketProvider } from "../contexts/SessionContext.tsx";
 import "./global.css";
 
 function App() {
-  return (
-    <SocketProvider>
-      <RouterProvider router={router}></RouterProvider>
-    </SocketProvider>
-  );
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
--- apps/client/src/app/Layout.tsx
+++ apps/client/src/app/Layout.tsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router";
 import ServerStatus from "@/components/ServerStatus.tsx";
+import { SocketProvider } from "@/contexts/SessionContext.tsx";
 
 function Layout() {
   return (
     <div className="flex h-screen flex-col items-center">
-      <ServerStatus />
-      <Outlet />
+      <SocketProvider>
+        <ServerStatus />
+        <Outlet />
+      </SocketProvider>
     </div>
   );
 }
